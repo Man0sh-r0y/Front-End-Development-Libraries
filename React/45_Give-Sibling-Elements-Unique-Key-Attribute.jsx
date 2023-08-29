3534af62db933c07f9222fcc204bda8c51dbde63
Manash Roy <manashroy454@gmail.com>
2023-08-29 18:08:07 +0530

    Added Files

--- React/45_Give-Sibling-Elements-Unique-Key-Attribute.jsx
+++ React/45_Give-Sibling-Elements-Unique-Key-Attribute.jsx
@@ -8,7 +8,10 @@ const frontEndFrameworks = [
 ];
 
 function Frameworks() {
-  const renderFrameworks = null; // Change this line
+  // const renderFrameworks = null; // Change this line
+  const renderFrameworks = frontEndFrameworks.map((item, index) => {
+    return <li key={index}>{item}</li>;
+  });
   return (
     <div>
       <h1>Popular Front End JavaScript Frameworks</h1>
